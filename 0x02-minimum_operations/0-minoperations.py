7cbd2094ce0d26f12f254f3313f70861465eb99c
Pule <proceedingmc@gmail.com>
2024-03-20 10:40:54 +0200

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,44 +2,23 @@
 
 def minOperations(n):
     """
-    Calculates the minimum number of operations required to
-    obtain a file of 'n' characters,
-    starting with a file of 1 character and using
-    the operation of copying and pasting.
-
-    Args:
-    n (int): The desired number of characters in the file.
-
-    Returns:
-    int: The minimum number of operations required.
+    Calculates the fewest number of operations needed
+    to result in exactly n H characters in the file.
     """
-    chars_in_file = 1
-    # Initialize the number of characters in the file
-    no_of_times_copied = 0
-    # Initialize the number of times the file is copied
-    counter = 0
-    # Initialize the counter for the number of operations
+    # If n is less than or equal to 1, it's impossible to achieve, return 0
+    if n <= 1:
+        return 0
 
-    # Loop until the number of characters in the file reaches 'n'
-    while chars_in_file < n:
-        remainder = n - chars_in_file
-        # Calculate the remaining characters needed
+    operations = 0  # Initialize the number of operations needed
+    i = 2  # Start from 2 as the smallest prime factor
 
-        # Check if the remaining characters can
-        # be evenly divided by the current number
-        # of characters in the file
-        if (remainder % chars_in_file == 0):
-            no_of_times_copied = chars_in_file
-            # Update the number of times the file is copied
-            chars_in_file += no_of_times_copied
-            # Update the number of characters in the file
-            counter += 2
-            # Increment the counter by 2 for the copy and paste operations
+    # Iterate from 2 to n
+    while i <= n:
+        # Check if i is a factor of n
+        if n % i == 0:
+            n //= i  # Divide n by i
+            operations += i  # Add i to the total operations needed
         else:
-            chars_in_file += no_of_times_copied
-            # Update the number of characters in the file
-            counter += 1
-            # Increment the counter by 1 for the paste operation
+            i += 1  # Move to the next number if i is not a factor
 
-    return counter
-    # Return the total number of operations required
+    return operations  # Return the total number of operations
