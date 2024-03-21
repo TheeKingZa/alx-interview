8b6a5aed4c44ef3f1206f93db31af45fa15e6131
Pule <proceedingmc@gmail.com>
2024-03-21 20:45:16 +0200

    still editing

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,30 +1,78 @@
 #!/usr/bin/python3
+'''
+Minimum Operations Python3 Challenge:
+This script calculates the fewest number
+of operations needed to result in exactly n H
+characters in the file.
+'''
+
+
 def minOperations(n):
-    """
-    Calculate the fewest number of operations needed
-    to result in exactly n H characters in the file.
+    '''
+    Calculates the fewest number of operations needed to result in exactly n H
+    characters in the file.
 
     Args:
-    - n: integer, the target number of H characters
+        n (int): The target number of H characters.
 
     Returns:
-    - integer, the minimum number of operations
-    """
+        int: The minimum number of operations.
+        If n is impossible to achieve, return 0.
+    '''
 
-    if n <= 1:
-        return 0
+    # Initialize variables
+    pasted_chars = 1  # Represents the current number of characters in the file
+    clipboard = 0     # Represents the number of H's copied to the clipboard
+    counter = 0       # Counts the number of operations
 
-    operations = [0] * (n + 1)
+    # Loop until the desired number of characters is achieved
+    while pasted_chars < n:
+        # If nothing is copied yet, perform a "Copy All" operation
+        if clipboard == 0:
+            clipboard = pasted_chars
+            # Copy the current number of characters
+            counter += 1
+            # Increment the operation counter
 
-    for i in range(2, n + 1):
-        # Start with the assumption that the minimum operations for i is i.
-        operations[i] = i
-        for j in range(2, i):
-            if i % j == 0:
-                # If i is divisible by j, then we can perform j operations
-                # to get i by copying j times and pasting.
-                operations[i] = min(operations[i], operations[j] + i // j)
-                # We also consider the case where we
-                # paste i//j times after copying j characters.
+        # If only one character is pasted, perform a "Paste" operation
+        if pasted_chars == 1:
+            pasted_chars += clipboard
+            # Paste the characters from the clipboard
+            counter += 1
+            # Increment the operation counter
+            continue
+            # Continue to the next iteration
 
-    return operations[n]
+        # Calculate the remaining characters needed to reach the target
+        remaining = n - pasted_chars
+
+        # If the remaining characters are less
+        # than the characters in the clipboard,
+        # it's impossible to achieve the target number of characters
+        if remaining < clipboard:
+            return 0
+
+        # If the remaining characters cannot
+        # be evenly divided by the current characters,
+        # paste the characters from the clipboard
+        if remaining % pasted_chars != 0:
+            pasted_chars += clipboard
+            # Paste the characters from the clipboard
+            counter += 1
+            # Increment the operation counter
+        else:
+            # If the remaining characters can
+            # be evenly divided by the current characters,
+            # perform a "Copy All" operation followed by a "Paste" operation
+            clipboard = pasted_chars
+            # Copy the current number of characters
+            pasted_chars += clipboard
+            # Paste the characters from the clipboard
+            counter += 2
+            # Increment the operation counter
+
+    # Check if the desired number of characters is achieved
+    if pasted_chars == n:
+        return counter  # Return the total number of operations
+    else:
+        return 0        # Return 0 if the target is not achieved
