e692c5047fa79237d5712aab77d65d88b3b576d4
Pule <proceedingmc@gmail.com>
2024-04-26 10:53:20 +0200

    try 3

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
+"""
+Rotate 2D Matrix
+"""
 
 def rotate_2d_matrix(matrix):
     """
@@ -12,31 +14,12 @@ def rotate_2d_matrix(matrix):
     - None
     """
     n = len(matrix)  # Get the size of the matrix
-    # Traverse the upper triangle of the matrix
-    for i in range(n):
-        # Start from the diagonal and traverse to the end
-        for j in range(i, n):
-            # Swap the elements across the diagonal
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-
-def reverse_matrix(matrix):
-    # Reverse each row of the matrix
-    for row in matrix:
-        row.reverse()
-
-def rotate_2d_matrix(matrix):
-    n = len(matrix)  # Get the size of the matrix
 
     # Transpose the matrix
-    transpose_matrix(matrix, n)
-
-    # Reverse the transposed matrix
-    reverse_matrix(matrix)
-
-    return matrix
-
-def transpose_matrix(matrix, n):
-    # Transpose the matrix by swapping elements across the diagonal
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Reverse each row of the transposed matrix
+    for row in matrix:
+        row.reverse()
