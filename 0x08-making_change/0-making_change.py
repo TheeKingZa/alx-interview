ef9a307940cb4a33c5331511a67972ac8532a643
Pule <proceedingmc@gmail.com>
2024-05-02 19:28:24 +0200

    making a change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -19,30 +19,16 @@ def makeChange(coins, total):
     # If the total amount is non-positive, return 0 coins needed
     if total <= 0:
         return 0
-
-    # Initialize a table to store minimum coins needed
-    # for each amount from 0 to total
     table = [sys.maxsize for i in range(total + 1)]
-    table[0] = 0  # Base case: 0 coins needed for 0 amount
-
-    # Iterate over each amount from 1 to total
+    table[0] = 0
+    m = len(coins)
     for i in range(1, total + 1):
-        # Iterate over each coin denomination
-        for j in range(len(coins)):
-            # If the coin value is less than or equal to the current amount
+        for j in range(m):
             if coins[j] <= i:
-                # Calculate the minimum coins needed for the
-                # remaining amount after using this coin
                 subres = table[i - coins[j]]
-                # Update the table if using this coin
-                # leads to a smaller number of coins needed
                 if subres != sys.maxsize and subres + 1 < table[i]:
                     table[i] = subres + 1
 
-    # If it's impossible to make the total amount with
-    # the given coins, return -1
     if table[total] == sys.maxsize:
         return -1
-
-    # Otherwise, return the minimum number of coins needed for the total amount
     return table[total]
